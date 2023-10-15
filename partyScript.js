01f78c5887629f253f59bd6adabababc0f748125
Gelatino <miguel.guzman@zalando.de>
2023-10-15 16:02:39 +0200

    added the yaml

--- partyScript.js
+++ partyScript.js
@@ -1,31 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var targetDate = new Date("October 20, 2023 21:00:00").getTime();
-
-    var countdownInterval = setInterval(function() {
-        var now = new Date().getTime();
-        var distance = targetDate - now;
-
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        document.getElementById("days").textContent = String(days).padStart(2, '0');
-        document.getElementById("hours").textContent = String(hours).padStart(2, '0');
-        document.getElementById("minutes").textContent = String(minutes).padStart(2, '0');
-        document.getElementById("seconds").textContent = String(seconds).padStart(2, '0');
-
-        if (distance < 0) {
-            clearInterval(countdownInterval);
-            document.getElementById("countdown").innerHTML = "The time has come!";
-        }
-    }, 1000);
-});
-
-
 // Replace with your own Giphy API key
-const API_KEY = 'YOUR_GIPHY_API_KEY';
-const endpoint = `https://api.giphy.com/v1/gifs/random?api_key=${GIPHY_API_KEY}`;
+const API_KEY = 'YOUR_API_KEY_PLACEHOLDER';
+const endpoint = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
 
 
 fetch(endpoint)
